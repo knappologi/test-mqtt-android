f12ccab8f9bd0469198445afc812970a894a2d90
kanppologi <nejfredrika@gmail.com>
2019-10-13 15:56:59 +0200

    add subscribe

--- app/src/main/java/com/example/test_mqtt/MainActivity.java
+++ app/src/main/java/com/example/test_mqtt/MainActivity.java
@@ -16,6 +16,8 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     MqttAndroidClient client;
@@ -28,12 +30,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        client.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+
+                if (reconnect) {
+                    System.out.println("Reconnected to : " + serverURI);
+                    // Because Clean Session is true, we need to re-subscribe
+                     subscribe();
+                } else {
+                    System.out.println("Wow! Connected to: " + serverURI);
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                System.out.println("The Connection was lost.");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                System.out.println("Incoming message: " + new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
         run();
     }
 
     private void run() {
         connect();
-
     }
 
     private void connect3(){
@@ -108,6 +138,7 @@ public class MainActivity extends AppCompatActivity {
                     // We are connected
                     Log.d(TAG, "onSuccess");
                     System.out.println(TAG + " Success. Connected to " + serverUri);
+                   // subscribe();
                 }
 
                 @Override
@@ -115,6 +146,33 @@ public class MainActivity extends AppCompatActivity {
                     // Something went wrong e.g. connection timeout or firewall problems
                     Log.d(TAG, "onFailure");
                     System.out.println(TAG + " Oh no! Failed to connect to "  + serverUri);
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void subscribe() {
+        final String topic = "hellocutie0";
+        int qos = 1;
+        try {
+            System.out.println("HELLO THIS IS CLIENT SUBSCRIBE: " + client);
+            IMqttToken subToken = client.subscribe(topic, qos);
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    System.out.println("Subscription successful to topic: " + topic);
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    System.out.println("Failed to subscribe to topic: " + topic);
+                    // The subscription could not be performed, maybe the user was not
+                    // authorized to subscribe on the specified topic e.g. using wildcards
 
                 }
             });
